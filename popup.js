a045aea3717b7ea0f3cec9248a94711609f96d38
nameisxi <tuomas.j.j.peltonen@gmail.com>
2018-03-11 16:57:47 +0200

    Added error handling and enter key event listener

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
 document.getElementById("answer").style.visibility = "hidden";
 
-document.getElementById('searchButton').addEventListener('click', () => {
-    function getWords(path, callback) {
-        return new Promise((resolve, reject) => {
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(xhr.responseText);
-                    } else {
-                        reject(xhr);
-                    }
+function getWords(path, callback) {
+    return new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    resolve(xhr.responseText);
+                } else {
+                    reject(xhr);
                 }
-            };
-            xhr.open("GET", path);
-            xhr.send();
-        });
-    }
+            }
+        };
+        xhr.open("GET", path);
+        xhr.send();
+    });
+}
 
+function searchWord() {
     getWords(chrome.runtime.getURL("/dictionary/sanalista.txt"))
         .then(function (fileData) {
             var wordList = fileData.split("\n");
@@ -25,21 +25,32 @@ document.getElementById('searchButton').addEventListener('click', () => {
 
             if (!(wordToFind == '') && !(wordToFind == undefined)) {
                 var wordToFindPieces = wordToFind.split(" ");
-                var wordToFindWithHyphen = wordToFindPieces[0] + "-" + wordToFindPieces[1];
-                var wordToFindWithoutSpace = wordToFindPieces[0] + wordToFindPieces[1];
 
-                if (wordList.includes(wordToFindWithHyphen)) {
-                    document.getElementById("answer").innerHTML = "Sana " + wordToFindWithHyphen + " on yhdyssana";
-                    document.getElementById("answer").style.color = "#00BFFE";
-                    document.getElementById("answer").style.visibility = "visible";
-                } else if (wordList.includes(wordToFindWithoutSpace)) {
-                    document.getElementById("answer").innerHTML = "Sana " + wordToFindWithoutSpace + " on yhdyssana";
-                    document.getElementById("answer").style.color = "#00BFFE";
-                    document.getElementById("answer").style.visibility = "visible";
-                } else {
-                    document.getElementById("answer").innerHTML = "Sanat " + wordToFindPieces[0] + " ja " + wordToFindPieces[1] + " eivät muodosta yhdyssanaa";
+                if (wordToFindPieces.length < 2) {
+                    document.getElementById("answer").innerHTML = "Et syöttänyt kahta sanaa";
                     document.getElementById("answer").style.color = "#AB0028";
                     document.getElementById("answer").style.visibility = "visible";
+                } else if (wordToFindPieces.length > 2) {
+                    document.getElementById("answer").innerHTML = "Syötit enemmän kuin kaksi sanaa. Yhdyssana koostuu kahdesta sanasta.";
+                    document.getElementById("answer").style.color = "#AB0028";
+                    document.getElementById("answer").style.visibility = "visible";
+                } else {
+                    var wordToFindWithHyphen = wordToFindPieces[0] + "-" + wordToFindPieces[1];
+                    var wordToFindWithoutSpace = wordToFindPieces[0] + wordToFindPieces[1];
+
+                    if (wordList.includes(wordToFindWithHyphen)) {
+                        document.getElementById("answer").innerHTML = "Sana " + wordToFindWithHyphen + " on yhdyssana";
+                        document.getElementById("answer").style.color = "#00BFFE";
+                        document.getElementById("answer").style.visibility = "visible";
+                    } else if (wordList.includes(wordToFindWithoutSpace)) {
+                        document.getElementById("answer").innerHTML = "Sana " + wordToFindWithoutSpace + " on yhdyssana";
+                        document.getElementById("answer").style.color = "#00BFFE";
+                        document.getElementById("answer").style.visibility = "visible";
+                    } else {
+                        document.getElementById("answer").innerHTML = "Sanat " + wordToFindPieces[0] + " ja " + wordToFindPieces[1] + " eivät muodosta yhdyssanaa";
+                        document.getElementById("answer").style.color = "#AB0028";
+                        document.getElementById("answer").style.visibility = "visible";
+                    }
                 }
             } else {
                 document.getElementById("answer").innerHTML = "Et syöttänyt sanaa";
@@ -50,4 +61,15 @@ document.getElementById('searchButton').addEventListener('click', () => {
         .catch(function (xhr) {
             console.log("XHR Error");
         });
-});       
+}
+
+document.getElementById('searchButton').addEventListener('click', () => {
+    this.searchWord();
+});
+
+document.getElementById('searchField').addEventListener('keypress', (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault();
+        this.searchWord();
+    }
+}); 
